33f02a0b06bbe5d16d0929d32713375fc0ad23f5
Hadi Chokr <hadichokr@icloud.com>
2025-04-28 14:22:12 +0200

    Update post_install.sh

--- arkdep-build.d/my-arkane-build/extensions/post_install.sh
+++ arkdep-build.d/my-arkane-build/extensions/post_install.sh
@@ -9,10 +9,14 @@ arch-chroot "$workdir" pacman -Sy --noconfirm --needed base-devel git
 printf '\e[1;32m-->\e[0m\e[1m Creating temporary user\e[0m\n'
 arch-chroot "$workdir" useradd aur -m -p '!'
 
+# Allow 'aur' to use sudo without password
+printf '\e[1;32m-->\e[0m\e[1m Allowing aur user passwordless sudo\e[0m\n'
+arch-chroot "$workdir" bash -c "echo 'aur ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/aur"
+
 # Install yay manually first (because we need it to install others)
 printf '\e[1;32m-->\e[0m\e[1m Bootstrapping yay-bin\e[0m\n'
 arch-chroot -u aur:aur "$workdir" bash -c "
-  cd /home/aur && 
+  cd /home/aur &&
   git clone https://aur.archlinux.org/yay-bin.git &&
   cd yay-bin &&
   makepkg -si --noconfirm
@@ -24,6 +28,10 @@ for package in "${aur_packages[@]}"; do
     arch-chroot -u aur:aur "$workdir" bash -c "yay -S --noconfirm $package"
 done
 
-# Cleanup
+# Cleanup sudoers file
+printf '\e[1;32m-->\e[0m\e[1m Removing temporary sudoers rule\e[0m\n'
+arch-chroot "$workdir" rm -f /etc/sudoers.d/aur
+
+# Cleanup user
 printf '\e[1;32m-->\e[0m\e[1m Performing cleanup\e[0m\n'
 arch-chroot "$workdir" userdel -r aur
